cb1164819d30d04061d6eadbf9358695350f3518
LiviaMachadoF <liviamf10@gmail.com>
2024-03-21 18:48:46 -0300

    CursoJS

---
+++ Curso JS/aula11/ex008.js
@@ -0,0 +1,9 @@
+//console.log('teste para ver se escrevi direitinho no f8')
+
+var vel = 61
+console.log(`A velocidade do carro é ${vel}km/h`)
+//condição simples
+if (vel > 60) {  
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
